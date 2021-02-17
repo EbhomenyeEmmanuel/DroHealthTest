488ebc83172041105e9913d7cd1681505fab5a84
Ebhomenye Emmanuel <eebhomenye@yahoo.com>
2021-02-17 16:12:45 +0100

    Glide data Binding Adapter created.

---
+++ app/src/main/java/com/esq/drohealthtest/databinding/GlideBindingAdapter.kt
@@ -0,0 +1,23 @@
+package com.esq.drohealthtest.databinding
+
+import android.content.Context
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.esq.drohealthtest.R
+
+object GlideBindingAdapter {
+    @BindingAdapter("imageUrl")
+    fun setImage(view: ImageView, imageUrl: Int) {
+        val context: Context = view.context
+        val options: RequestOptions = RequestOptions()
+            .placeholder(R.drawable.ic_launcher_background)
+            .error(R.drawable.ic_launcher_background)
+
+        Glide.with(context)
+            .setDefaultRequestOptions(options)
+            .load(imageUrl)
+            .into(view)
+    }
+}
