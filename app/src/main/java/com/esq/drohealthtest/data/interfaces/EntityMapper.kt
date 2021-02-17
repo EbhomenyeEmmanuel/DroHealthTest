941b8eaf1ce620e66074f67e8ac87d19820dd4be
Ebhomenye Emmanuel <eebhomenye@yahoo.com>
2021-02-17 16:14:08 +0100

    Interfaces created.

---
+++ app/src/main/java/com/esq/drohealthtest/data/interfaces/EntityMapper.kt
@@ -0,0 +1,8 @@
+package com.esq.drohealthtest.data.interfaces
+
+interface EntityMapper <Entity, DomainModel> {
+
+    fun mapFromEntity(entity: Entity): DomainModel
+
+    fun mapToEntity (domainModel: DomainModel) : Entity
+}
---
+++ app/src/main/java/com/esq/drohealthtest/data/interfaces/StoreRepository.kt
@@ -0,0 +1,11 @@
+package com.esq.drohealthtest.data.interfaces
+
+import androidx.lifecycle.LiveData
+import com.esq.drohealthtest.data.model.StoreItem
+import kotlinx.coroutines.flow.Flow
+
+interface StoreRepository {
+    fun getHomeListResultStream(): Flow<List<StoreItem>>
+    fun getNumberOfItemsInStore(): LiveData<Int>
+    fun getNumberOfItemsInBag(): LiveData<Int>
+}
