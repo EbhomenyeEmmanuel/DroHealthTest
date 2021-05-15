b365f6af65f95c21e063ba364c731e5749d9b510
Ebhomenye Emmanuel <eebhomenye@yahoo.com>
2021-05-15 21:20:51 +0100

    Store Screen UI State used with MutableStateFlow. Navigation of Drug Screen used with an Event.  displayInitialData() giving an error.

--- app/src/main/java/com/esq/drohealthtest/ui/storescreen/StoreScreenViewModel.kt
+++ app/src/main/java/com/esq/drohealthtest/ui/storescreen/StoreScreenViewModel.kt
@@ -3,32 +3,78 @@ package com.esq.drohealthtest.ui.storescreen
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.esq.drohealthtest.data.interfaces.StoreRepository
 import com.esq.drohealthtest.data.model.StoreItem
+import com.esq.drohealthtest.data.model.StoreScreenUiState
 import com.esq.drohealthtest.utils.Event
+import com.esq.drohealthtest.utils.toFlow
 import dagger.hilt.android.lifecycle.HiltViewModel
-import dagger.hilt.android.scopes.ViewModelScoped
-import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
+@ExperimentalCoroutinesApi
 @HiltViewModel
-class StoreScreenViewModel @Inject constructor(private val repository: StoreRepository) : ViewModel() {
+class StoreScreenViewModel @Inject constructor(
+    private val repository: StoreRepository
+) : ViewModel() {
+    val TAG = this::class.java.simpleName
 
-    val storeItems: Flow<List<StoreItem>> = repository.getHomeListResultStream()
+    private val _currentStoreScreenState =
+        MutableStateFlow<StoreScreenUiState>(StoreScreenUiState.Empty)
+    val currentStoreScreenState: StateFlow<StoreScreenUiState> = _currentStoreScreenState
 
+    init {
+        viewModelScope.launch {
+            displayInitialData()
+            getStoreItems()
+        }
+    }
+
+    private val _numberOfItemsInStore = MutableLiveData<Int>()
     val numberOfItemsInStore: LiveData<Int>
-        get() = repository.getNumberOfItemsInStore()
+        get() = _numberOfItemsInStore
 
-    val numberOfItemsInBag: LiveData<Int>
-        get() = repository.getNumberOfItemsInBag()
+    private val _navigateToViewDrugScreen = MutableLiveData<Event<StoreItem>>()
 
-    private val _navigateToViewArticle = MutableLiveData<Event<StoreItem>>()
+    val navigateToViewDrugScreen: LiveData<Event<StoreItem>>
+        get() = _navigateToViewDrugScreen
 
-    val navigateToViewArticle: LiveData<Event<StoreItem>>
-        get() = _navigateToViewArticle
+    private fun displayInitialData() {
+        //_numberOfItemsInStore.value = repository.getNumberOfItemsInStore().value
+    }
 
-    fun onViewHomeArticleClicked(item: StoreItem) {
-        _navigateToViewArticle.value = Event(item)
+    private suspend fun getStoreItems() {
+        withContext(Dispatchers.IO){
+            _currentStoreScreenState.value = StoreScreenUiState.Loading
+            delay(1L)
+            _currentStoreScreenState.value =
+                StoreScreenUiState.Success(repository.getHomeListResultStream())
+        }
+    }
+
+    //Navigate to Drug Screen when clicked
+    fun onViewDrugClicked(item: StoreItem) {
+        _navigateToViewDrugScreen.value = Event(item)
+    }
+
+    //Search from db
+    fun searchResults(queryValue: String) {
+        viewModelScope.launch {
+            repository.getSearchResultStream().map {
+                var searchResultList = mutableListOf<StoreItem>()
+                for (item in it) {
+                    if (item.mainName == queryValue || item.medicineTypeName == queryValue || item.otherName == queryValue) {
+                        searchResultList.add(item)
+                        _currentStoreScreenState.value =
+                            StoreScreenUiState.Success(searchResultList.toFlow())
+                    }
+                }
+            }
+        }
     }
 
 }
