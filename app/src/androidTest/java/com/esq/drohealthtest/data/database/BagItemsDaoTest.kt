7e2020a0e971f7b67b56f7aa422c916f54e5f6ff
Ebhomenye Emmanuel <eebhomenye@yahoo.com>
2021-05-15 13:22:21 +0100

    Insert and delete tests were passed.

--- app/src/androidTest/java/com/esq/drohealthtest/data/database/BagItemsDaoTest.kt
+++ app/src/androidTest/java/com/esq/drohealthtest/data/database/BagItemsDaoTest.kt
@@ -1,17 +1,17 @@
 package com.esq.drohealthtest.data.database
 
+import android.util.Log
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.lifecycle.asLiveData
 import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.SmallTest
 import com.esq.drohealthtest.R
-import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.esq.drohealthtest.data.model.BagItemDatabaseModel
 import com.esq.drohealthtest.getOrAwaitValue
 import com.google.common.truth.Truth.assertThat
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.test.runBlockingTest
 import org.junit.After
 import org.junit.Before
@@ -19,36 +19,75 @@ import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
+@ExperimentalCoroutinesApi
 @RunWith(AndroidJUnit4::class)
 @SmallTest
 class BagItemsDaoTest {
     private lateinit var database: DroHealthStoreDatabase
     private lateinit var dao: BagItemsDao
+
     @get:Rule
     var instantExecutorRule = InstantTaskExecutorRule()
 
     @Before
     fun setUp() {
-        database = Room.inMemoryDatabaseBuilder(
-            ApplicationProvider.getApplicationContext(), DroHealthStoreDatabase::class.java
-        ).allowMainThreadQueries().build()
+        try {
+            database = Room.inMemoryDatabaseBuilder(
+                ApplicationProvider.getApplicationContext(), DroHealthStoreDatabase::class.java
+            ).allowMainThreadQueries().build()
+        } catch (e: Exception) {
+            Log.i(this.javaClass.simpleName, e.message!!)
+        }
         dao = database.bagItemsDao()
     }
 
     @After
-    fun tearDown(){
+    fun tearDown() {
         database.close()
     }
 
-    @ExperimentalCoroutinesApi
     @Test
-    fun insertBagItem() = runBlockingTest {
-        val bagItem = BagItemDatabaseModel(id = 1,drugIcon = R.drawable.ic_launcher_background, drugName = "Folic Acid (100)", drugType = "Tablet - 5mg", drugPrice = "170", drugQuantity = 1)
-        val bagItem2 = BagItemDatabaseModel(id = 2,drugIcon = R.drawable.ic_launcher_background, drugName = "Folic Acid (100)", drugType = "Tablet - 5mg", drugPrice = "170", drugQuantity = 1)
+    fun testInsertBagItem() = runBlockingTest {
+        val bagItem = BagItemDatabaseModel(
+            id = 1,
+            drugIcon = R.drawable.ic_launcher_background,
+            drugName = "Folic Acid (100)",
+            drugType = "Tablet - 5mg",
+            drugPrice = "170",
+            drugQuantity = 1
+        )
+        val bagItem2 = BagItemDatabaseModel(
+            id = 2,
+            drugIcon = R.drawable.ic_launcher_background,
+            drugName = "Folic Acid (100)",
+            drugType = "Tablet - 5mg",
+            drugPrice = "170",
+            drugQuantity = 1
+        )
 
         dao.saveBagItem(bagItem)
         val allBagItems = dao.getAllItemsInBag().asLiveData().getOrAwaitValue()
         assertThat(bagItem).isIn(allBagItems)
+
+        val noOfBagItems = dao.getNumberOfItemsInBag().asLiveData().getOrAwaitValue()
+        assertThat(noOfBagItems).isEqualTo(1)
+    }
+
+    @Test
+    fun testClearBagItems() = runBlockingTest {
+        val bagItem2 = BagItemDatabaseModel(
+            id = 2,
+            drugIcon = R.drawable.ic_launcher_background,
+            drugName = "Folic Acid (100)",
+            drugType = "Tablet - 5mg",
+            drugPrice = "170",
+            drugQuantity = 1
+        )
+
+        dao.saveBagItem(bagItem2)
+        dao.deleteAll()
+        val noOfBagItems = dao.getNumberOfItemsInBag().asLiveData().getOrAwaitValue()
+        assertThat(noOfBagItems).isEqualTo(0)
     }
 
 }
