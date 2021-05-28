f01c92016b82f19ddb34c9f05dcab71bae7571d8
Ebhomenye Emmanuel <eebhomenye@yahoo.com>
2021-05-29 00:25:38 +0100

    Class converted to parcelable

--- app/src/main/java/com/esq/drohealthtest/data/model/BagItem.kt
+++ app/src/main/java/com/esq/drohealthtest/data/model/BagItem.kt
@@ -1,8 +1,15 @@
 package com.esq.drohealthtest.data.model
 
+import android.annotation.SuppressLint
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
 /**
  * Data class that captures user information for bag items saved in local database
  */
+
+@Parcelize
+@SuppressLint("ParcelCreator")
 data class BagItem(
     val id: Int,
     val drugIcon: Int,
@@ -10,4 +17,4 @@ data class BagItem(
     val drugType: String,
     val drugPrice: String,
     var drugQuantity: Int
-)
+) : Parcelable
