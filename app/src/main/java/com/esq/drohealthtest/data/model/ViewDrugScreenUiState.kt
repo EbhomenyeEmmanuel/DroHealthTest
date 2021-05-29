0a6667c3ebb32317273fc16f15c57dfec8a088e0
Ebhomenye Emmanuel <eebhomenye@yahoo.com>
2021-05-29 01:29:52 +0100

    Constructor object changed.

--- app/src/main/java/com/esq/drohealthtest/data/model/ViewDrugScreenUiState.kt
+++ app/src/main/java/com/esq/drohealthtest/data/model/ViewDrugScreenUiState.kt
@@ -2,12 +2,12 @@ package com.esq.drohealthtest.data.model
 
 
 /**
- * ViewDrugScreen state added.
+ * ViewDrugScreen UI state.
  */
 sealed class ViewDrugScreenUiState{
-    data class Success(val successMessageHeader: String, val successMessageSubtitle: String) :
+    data class Success(val dialogMessageData: DialogMessageData) :
         ViewDrugScreenUiState()
-    data class Error(val errorMessageHeader: String, val errorMessageSubtitle: String) :
+    data class Error(val dialogMessageData: DialogMessageData) :
         ViewDrugScreenUiState()
     object Empty : ViewDrugScreenUiState()
 }
